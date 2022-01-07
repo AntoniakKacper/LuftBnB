71d3d1e135b224d8d758b21c43e412f48ff4a022
AntoniakKacper <kacper.antoniak2@gmail.com>
2022-01-07 17:41:57 +0100

    Changes by Daniel

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export {default as TextInput} from './TextInput/TextInput';
 export {default as BottomNavbar} from './BottomNavbar/BottomNavbar';
 export {default as Navbar} from './Navbar/Navbar';
 export {default as PasswordInput} from './PasswordInput/PasswordInput';
+export {default as CustomSelect} from './Select/CustomSelect';
